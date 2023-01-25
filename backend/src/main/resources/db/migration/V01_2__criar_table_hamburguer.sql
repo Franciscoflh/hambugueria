d6991e1b6e741342cbc81ecf2d64ac8dfea53714
lfrferreira <lfrferreira@tjma.jus.br>
2023-01-25 11:05:50 -0300

    Delete migration if dont worked

--- backend/src/main/resources/db/migration/V01_2__criar_table_hamburguer.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE hambuguer
-(
-    codigo_burguer INT AUTO_INCREMENT NOT NULL,
-    nome           VARCHAR(100)       NULL,
-    `descrição`    VARCHAR(255)       NULL,
-    preco          INT                NULL,
-    ingredientes   VARCHAR(255)       NULL,
-    CONSTRAINT pk_hambuguer PRIMARY KEY (codigo_burguer)
-);
