9587d4a8cee01730d0ce289f3b4184bfcdaf88c1
lfrferreira <lfrferreira@tjma.jus.br>
2023-01-27 11:05:31 -0300

    clean trash and imports dont used

--- backend/src/main/java/com/am/hambuqueria/api/dto/mapper/UsuarioMapperDTO.java
+++ backend/src/main/java/com/am/hambuqueria/api/dto/mapper/UsuarioMapperDTO.java
@@ -1,16 +1,7 @@
 package com.am.hambuqueria.api.dto.mapper;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Component;
-
-import com.am.hambuqueria.api.dto.request.UsuarioRequestDTO;
-import com.am.hambuqueria.api.dto.response.UsuarioResponseDTO;
-import com.am.hambuqueria.domain.model.Usuario;
-
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
