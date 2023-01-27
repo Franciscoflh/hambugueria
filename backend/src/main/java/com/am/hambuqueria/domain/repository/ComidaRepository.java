db4e2348392a32f040d5db60d44480698e3785ca
lfrferreira <lfrferreira@tjma.jus.br>
2023-01-27 12:13:45 -0300

    Add finder to comida by tipo

--- backend/src/main/java/com/am/hambuqueria/domain/repository/ComidaRepository.java
+++ backend/src/main/java/com/am/hambuqueria/domain/repository/ComidaRepository.java
@@ -1,6 +1,7 @@
 package com.am.hambuqueria.domain.repository;
 
 import com.am.hambuqueria.domain.model.Comida;
+import com.am.hambuqueria.domain.model.TipoComida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
@@ -14,5 +15,7 @@ public interface ComidaRepository extends JpaRepository<Comida, Integer> {
     List<Comida> findByNomeContaining(@NonNull String nome);
 
     List<Comida> findAll();
+
+    List<Comida> findByTipoComida(TipoComida tipoComida);
 
 }
